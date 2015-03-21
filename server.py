29648596f386346f0cd0e4b7d52c575a6d163145
Shohei Aoki <shoaok@gmail.com>
2015-03-21 13:05:52 +0000

    v0.0.1

--- server.py
+++ server.py
@@ -3,18 +3,20 @@ import commands
 import tornado.ioloop
 import tornado.web
 
-
 class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("waiting for RML...") 
+
     def post(self):
         self.HandleRml()
 
     def HandleRml(self):
-        dispPortCmd = 'ls -l /dev/tty.* | sed -e "s/^.*dev/\/dev/g" | sed -n 1p'
-        _,sport= commands.getstatusoutput(dispPortCmd)
-        rml = "hoge.rml"
-        sendRmlCmd = "cat " +rml+ " > "+sport
+        rml = self.get_argument("rml")
+	fout = open("output.rml","w")
+	fout.write(rml)
+        sendRmlCmd = "cat output.rml > /dev/ttyUSB0"
         print sendRmlCmd
-        #os.system(sendRmlCmd)
+        os.system(sendRmlCmd)
 
 application = tornado.web.Application([
     (r"/", MainHandler),
