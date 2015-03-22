9507d324ce959831aee4a14d154f2721c49b0143
Shohei Aoki <shoaok@gmail.com>
2015-03-22 10:45:03 +0900

    websocket implemented

--- websocket/client.py
+++ websocket/client.py
@@ -1,28 +1,31 @@
 #!/usr/bin/python
 import websocket
-import thread
-import time
+import os
 
+def on_open(ws):
+    print "ws opened"
+
+def on_close(ws):
+    print "### closed ###"
 
 def on_message(ws, message):
     print message
     fout = open("output.rml","w")
     fout.write(message)
     fout.close()
+    HandleRml()
 
 def on_error(ws, error):
     print error
 
-def on_close(ws):
-    print "### closed ###"
-
-def on_open(ws):
-    print "ws opened"
-
+def HandleRml():
+    sendRmlCmd = "cat output.rml > /dev/ttyUSB0"
+    print sendRmlCmd
+    os.system(sendRmlCmd)
 
 if __name__ == "__main__":
     websocket.enableTrace(True)
-    ws = websocket.WebSocketApp("ws://localhost:9090/ws",
+    ws = websocket.WebSocketApp("ws://133.242.160.150:9090/ws",
                                 on_message = on_message,
                                 on_error = on_error,
                                 on_close = on_close)
